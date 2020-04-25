08255ce31e10deab702a9cfecde434319bfcc59b
asnisapps <asnisapps@gmail.com>
2020-04-25 09:07:08 -0300

    Aula_02
    Teste Heroku

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
+// Controller
+const Users = require('../controller/Users');
+const usersController = new Users();
+
 // Instancia do router
 const router = express.Router();
 
-router.get('/:id', function(req,res) {
-    res.send(`Eu recebi o parametro ${req.params.id}`)
-})
+router.get('/:id', usersController.get);
 
 module.exports = router;
